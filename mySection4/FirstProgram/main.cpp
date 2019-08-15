ef407bd64688940d274b653997c36492e2075c7d
Ezequiel <ezgode@gmail.com>
2019-08-15 11:21:11 +0200

    first commit

---
+++ mySection4/FirstProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int var;
+	std::cout << "Enter your number between 1 and 100: ";
+	std::cin >> var;
+	std::cout << "Awesome, you gave me a " << var << std::endl << "Really!!!" << std::endl;
+	return 0;
+}
